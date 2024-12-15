cf639f66d83a850d1f4effff09839bee329e7cc9
vaughnte <vaughnte222@gmail.com>
2024-12-15 17:21:04 -0500

    updates

--- src/com/blueseer/edi/apiUtils.java
+++ src/com/blueseer/edi/apiUtils.java
@@ -2673,10 +2673,11 @@ public class apiUtils {
         if (isSignedAndEncrypted) {
           mp.addBodyPart(mbp);
         
-          mbp2.setContent(mp);
-          mbp2.addHeader("Content-Type", "multipart/signed; protocol=\"application/pkcs7-signature\"; boundary=" + "\"" + newboundary + "\"" + "; micalg=sha1");
-        //  mbp2.addHeader("Content-Disposition", "attachment; filename=smime.p7m");
-         // mbp.addHeader("Content-Type", "multipart/signed; protocol=\"application/pkcs7-signature\"; boundary=" + "\"" + newboundary + "\"" + "; micalg=sha1");
+       //   mbp2.setContent(mp);
+       //   mbp2.addHeader("Content-Type", "multipart/signed; protocol=\"application/pkcs7-signature\"; boundary=" + "\"" + newboundary + "\"" + "; micalg=sha1");
+       
+         //  mbp2.addHeader("Content-Disposition", "attachment; filename=smime.p7m");
+          mbp.addHeader("Content-type", "multipart/signed; protocol=\"application/pkcs7-signature\"; boundary=" + "\"" + newboundary + "\"" + "; micalg=sha1");
          // mbp2 = mbp;
           
           
@@ -2696,7 +2697,7 @@ public class apiUtils {
             }
         }  
           
-          signedAndEncrypteddata = encryptData(mbp2.getInputStream().readAllBytes(), encryptcertificate, tp[18]);
+          signedAndEncrypteddata = encryptData(mbp.getInputStream().readAllBytes(), encryptcertificate, tp[18]);
           
         }
         
