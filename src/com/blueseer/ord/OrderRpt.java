b7673bfb5d9cb6d71ccbf21dfb7d7ebd054cde1e
vaughnte <vaughnte222@gmail.com>
2025-03-16 10:04:06 -0400

    kb column fix

--- src/com/blueseer/ord/OrderRpt.java
+++ src/com/blueseer/ord/OrderRpt.java
@@ -409,7 +409,7 @@ public class OrderRpt extends javax.swing.JPanel {
                 res = st.executeQuery("select so_nbr, so_po, so_create_date, so_ord_date, " +
                         " cm_name, cms_plantcode, cms_name, so_due_date, " +
                         " sod_line, sod_item, sod_desc, '' as msku, sod_custitem, sod_char1, " +
-                        " sod_uom, sod_ord_qty, sod_netprice as mskuprice, sod_char2 from so_mstr " + 
+                        " sod_uom, sod_ord_qty, sod_netprice, sod_char2 from so_mstr " + 
                         " inner join sod_det on sod_nbr = so_nbr " +
                         " inner join cm_mstr on cm_code = so_cust " +
                         " inner join cms_det on cms_code = so_cust and cms_shipto = so_ship " +
