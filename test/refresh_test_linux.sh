fab380a9cddd5ea3703efa6f4464e5dd76cf3795
vaughnte <vaughnte222@gmail.com>
2025-10-16 14:52:43 -0400

    test script updates

---
+++ test/refresh_test_linux.sh
@@ -0,0 +1,31 @@
+!/bin/bash
+
+echo "refreshing BlueSeer test instance"
+
+currdir=$(basename "$PWD")
+if [[ $currdir != "test" ]];
+then
+        echo "must be executed from test directory"
+        exit
+fi
+
+if [[ -d "data" ]];
+then
+        echo "removing old data directory..."
+        rm -fR data
+fi
+
+echo "adding data directory..."
+cp -R ../sf/data data
+
+echo "creating bs.cfg file for sqlite data connection..."
+cp ../scripts/bs.cfg.sqlite.linux bs.cfg
+
+echo "creating .patch file..."
+cp ../scripts/.patch .patch
+
+echo ""
+echo ""
+echo "NOTE!!  you will need to adjust the property name 'JDK17dir' in the build.xml file "
+echo " for your JDK17 path. "
+echo "Assign the path to the parameter as (example) /some/path/to/jdk-17  "
