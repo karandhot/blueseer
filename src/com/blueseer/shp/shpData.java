36266b8d7e714b1242b656d22472fa67cce74ddc
vaughnte <vaughnte222@gmail.com>
2026-01-27 05:37:17 -0500

    updates

--- src/com/blueseer/shp/shpData.java
+++ src/com/blueseer/shp/shpData.java
@@ -1529,8 +1529,8 @@ public class shpData {
                         rowArray.put(res.getString("sh_shipdate"));
                         rowArray.put(res.getString("sh_po"));
                         rowArray.put(res.getString("sh_status"));
-                        rowArray.put(bsNumber(res.getString("qty")));
-                        rowArray.put(bsNumber(res.getString("price")));
+                        rowArray.put(bsNumber(res.getDouble("qty")));
+                        rowArray.put(bsNumber(res.getDouble("price")));
                         jsonarray.put(rowArray);
                     }
            }
