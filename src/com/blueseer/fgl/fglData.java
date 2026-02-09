4b1e35d9948c54e65372168fc283e2c12bf8fed7
vaughnte <vaughnte222@gmail.com>
2026-02-09 06:47:24 -0500

    updates

--- src/com/blueseer/fgl/fglData.java
+++ src/com/blueseer/fgl/fglData.java
@@ -6622,7 +6622,7 @@ public class fglData {
             ResultSet res = null;
             try {
 
-            res = st.executeQuery("select glicd_acct, ac_desc from glic_accts inner join ac_mstr on ac_id = glcid_acct where " +
+            res = st.executeQuery("select glicd_acct, ac_desc from glic_accts inner join ac_mstr on ac_id = glicd_acct where " +
                     " glicd_name = " + "'" + name + "'" +
                     " AND glicd_profile = " + "'" + profile + "'" +        
                     " AND glicd_type = " + "'" + type + "'" + ";");
