bfd04d6b4e7068f97604d240b5128fa4ae527c18
vaughnte <vaughnte222@gmail.com>
2026-03-31 16:23:12 -0400

    updates

--- src/com/blueseer/inv/invData.java
+++ src/com/blueseer/inv/invData.java
@@ -9234,6 +9234,7 @@ public class invData {
                 String returnstring = sendServerPost(list, "", null, "dataServINV");
                 DefaultMutableTreeNode r = new DefaultMutableTreeNode();
                 if (returnstring != null && ! returnstring.isBlank()) {
+                    System.out.println("HERE: " + returnstring);
                     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                  r = objectMapper.readValue(returnstring, DefaultMutableTreeNode.class); 
                 }
